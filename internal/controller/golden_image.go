3fdf44ee0930158e0c442b495438eaf162dee616
Nic Smith <nicholas.j.smith-2@student.uts.edu.au>
2026-07-31 09:31:59 +1000

    add a package upgrade before kicking off
    Signed-off-by: Nic Smith <nicholas.j.smith-2@student.uts.edu.au>

--- internal/controller/golden_image.go
+++ internal/controller/golden_image.go
@@ -659,6 +659,7 @@ export -f sudo
 
 # Install prerequisites
 apt-get update
+apt-get upgrade -y
 apt-get install -y wget curl git
 
 # Download and run official setup script
@@ -697,6 +698,7 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Install ROS 2
 apt-get update
+apt-get upgrade -y
 apt-get install -y software-properties-common curl gnupg lsb-release
 curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
